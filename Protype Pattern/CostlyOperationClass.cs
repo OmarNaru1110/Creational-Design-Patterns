830a259b7eea91b285e8f6dc990aff9af1588e21
OmarNaru <nadaelomr391@gmail.com>
2024-01-25 13:35:13 +0200

    Prototype pattern added

---
+++ Protype Pattern/CostlyOperationClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protype_Pattern
+{
+    public class CostlyOperationClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; } 
+        
+        public CostlyOperationClass ShallowCopy()
+        {
+            return (CostlyOperationClass) this.MemberwiseClone();
+        }
+        public CostlyOperationClass DeepCopy() 
+        {
+            var obj = this.MemberwiseClone() as CostlyOperationClass;
+            obj.Address = new Address();
+            return obj;
+        }
+        public override string ToString()
+        {
+            return $"id: {Id}\t name: {Name}\nOperation: {Address?.Street}/{Address?.City}/{Address?.Country}\n=======================================";
+        }
+    }
+}
--- Protype Pattern/Program.cs
+++ Protype Pattern/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region ShallowCopy
+            /*            var obj = new CostlyOperationClass { 
+                            Id = 1,Name="obj",Address = new Address{
+                                Street="alakma",City="hehia",Country="egypt" } };
+
+                        var obj2=obj.ShallowCopy();
+                        obj2.Id = 2;
+                        obj2.Name = "obj2";
+                        obj2.Address.Street = "blabla";
+                        //obj.operation.street will change too cuz it's copied as reference type
+                        Console.WriteLine(obj.ToString());
+                        Console.WriteLine(obj2.ToString());
+                        //restore old obj
+                        obj.Address.Street = "alakma";
+                        //make obj2.operation reference new address in memory
+                        obj2.Address = new Address { Street = "Subeih", City = "hehia", Country = "egypt" };
+                        Console.WriteLine(obj.ToString());
+                        Console.WriteLine(obj2.ToString());*/
+            #endregion
+
+            #region DeepCopy
+            /*            var obj = new CostlyOperationClass {Id=1,Name="obj",Address= 
+                            new Address { 
+                                Street="alakma", City="hehia", Country="egypt"
+                            } 
+                        };
+                        var obj2=obj.DeepCopy();
+                        Console.WriteLine(obj.ToString());
+                        Console.WriteLine(obj2.ToString());*/
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
